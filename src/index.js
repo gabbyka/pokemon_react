4b9af86f92d00d3c256b57e8f11d0ec3e5d7e5ca
gabbyka <gabbyka@gmail.com>
2018-12-21 14:14:09 +0100

    day 6 finished

--- src/index.js
+++ src/index.js
@@ -2,20 +2,41 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { unstable_createResource as createResource } from "react-cache";
 
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, info) {
+    console.error(error, info);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // You can render any custom fallback UI
+      return this.props.fallback ? (
+        this.props.fallback
+      ) : (
+        <div>Something went wrong.</div>
+      );
+    }
+
+    return this.props.children;
+  }
+}
+
 let PokemonCollectionResource = createResource(() =>
-  fetch("https://pokeapi.co/api/v2/pokemon/").then(res => res.json())
+  fetch("https://pokeapi.co/api/v2/pokemon-blaa/").then(res => res.json())
 );
 
 function PokemonListItem(props) {
   return <li {...props} />;
-}
-
-let PokemonDetailResource = createResource(() =>
-  fetch("https://pokeapi.co/api/v2/pokemon/1/").then(res => res.json())
-);
-
-function PokemonDetailItem(props) {
-  return <div {...props} />;
 }
 
 function PokemonList() {
@@ -28,18 +49,6 @@ function PokemonList() {
   );
 }
 
-function PokemonDetail() {
-  return (
-    <div>
-      {PokemonDetailResource.read().abilities.map(pokemon => (
-        <PokemonDetailItem>
-          {pokemon.slot} {pokemon.ability.name}
-        </PokemonDetailItem>
-      ))}
-    </div>
-  );
-}
-
 function App() {
   return (
     <div>
@@ -47,15 +56,13 @@ function App() {
         <span role="img" aria-label="React holiday">
           ⚛️🎄
         </span>
-        : Day 5
+        : Day 6
       </h1>
-      <React.Suspense fallback={<div>...loading Pokemon Detail!</div>}>
-        <PokemonDetail />
-      </React.Suspense>
-
-      <React.Suspense fallback={<div>...loading</div>}>
-        <PokemonList />
-      </React.Suspense>
+      <ErrorBoundary fallback={<div>Pokemon list loading broken</div>}>
+        <React.Suspense fallback={<div>...loading</div>}>
+          <PokemonList />
+        </React.Suspense>
+      </ErrorBoundary>
     </div>
   );
 }
